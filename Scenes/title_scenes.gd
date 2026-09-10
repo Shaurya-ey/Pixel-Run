d5ce1bebff919553d54e7853d78b5ec7811ef575
Shaurya Pandey <shauryaey246@gmail.com>
2026-09-10 21:13:21 +0530

    First working run!!

--- Scenes/title_scenes.gd
+++ Scenes/title_scenes.gd
@@ -7,12 +7,14 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func _on_button_pressed() -> void:
-	get_tree().quit()
+	pass
 
-func _on_start_game_pressed() -> void:
-	pass # Replace with function body.
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_scene.tscn")
+		 # Replace with function body.
--- timer_screen.gd
+++ timer_screen.gd
@@ -1,31 +1,31 @@
 extends Node2D
-@onready var heart_container: HBoxContainer = $HeartContainer/Heart
-@onready var heart: TextureRect = $HeartContainer/Heart
-@onready var heart_2: TextureRect = $HeartContainer/Heart2
-@onready var heart_3: TextureRect = $HeartContainer/Heart3
-@onready var heart_4: TextureRect = $HeartContainer/Heart4
-@onready var heart_5: TextureRect = $HeartContainer/Heart5
+@onready var heart_container: HBoxContainer = $TextureRect/HeartContainer
+@onready var heart: TextureRect = $TextureRect/HeartContainer/Heart
+@onready var heart_2: TextureRect = $TextureRect/HeartContainer/Heart2
+@onready var heart_3: TextureRect = $TextureRect/HeartContainer/Heart3
+@onready var heart_4: TextureRect = $TextureRect/HeartContainer/Heart4
+@onready var heart_5: TextureRect = $TextureRect/HeartContainer/Heart5
 @onready var level: RichTextLabel = $Level
 @onready var timer: RichTextLabel = $Timer
 
-var time
-	
+var time: float = 5.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await Timer(5.0)
-	
+
 	if Global.minigames_done < 3:
-		Global.minigames_done = Global.minigames_done + 1 
-		get_tree().change_scene_to_file("res://scenes/minigame_" + str(Global.minigames_done) + ".tscn")
+		Global.minigames_done = Global.minigames_done + 1
+		get_tree().change_scene_to_file("res://minigame_" + str(Global.minigames_done) + ".tscn")
 	else:
-		get_tree().change_scene_to_file("res://scenes/title_screen.tscn")	
-		
+		get_tree().change_scene_to_file("res://Scenes/title_scenes.tscn")
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	match Global.lives: # asks or checks if lives is equal to one of 
-#these values, cool hack. by the way this is a horrid way to illustrate the 
+func _process(_delta: float) -> void:
+	match Global.lives: # asks or checks if lives is equal to one of
+#these values, cool hack. by the way this is a horrid way to illustrate the
 #lives visually so later you can always find alternative code. Now, dw abt it.
 
 		4:
@@ -44,21 +44,21 @@ func _process(delta: float) -> void:
 			heart_4.hide()
 		0:
 			heart_container.hide() # just hides everything
-	
-	timer.text = str(time) # make ths text reflect the value of the time variable. this makes names easier. the str() converts the int to a String
-	level.text = "Level " + str(Global.minigames_done) # this tells you want minigame you're on using concatenation (google the word yo)
+
+	timer.text = "%.1f" % time # make the text reflect the value of the time variable nicely formatted
+	level.text = "Level " + str(Global.minigames_done) # this tells you what minigame you're on using concatenation (google the word yo)
 
 func Timer(start_time: float): # making a new function for timer countdown!
-	# we want the timer to go down, and when it reaches 0 it transitions 
+	# we want the timer to go down, and when it reaches 0 it transitions
 	# to the next scene!
-	
+
 	time = start_time # make the timer, which is reflected through the timer text, start at your desired number
-	
+
 	while time > 0.0: # run if timer hasnt reached 0
-		await wait(0.1) # asks script to wait on this function. the 'wait' name for the function does nothing here, as await is just telling the scrpit to wait for the function to complete before progressing
+		await wait(0.1) # asks script to wait on this function. the 'wait' name for the function does nothing here, as await is just telling the script to wait for the function to complete before progressing
 		time -= 0.1 # remove 0.1
 		# progressively get the value smaller and smaller
-	
+
 	#when timer reaches 0
 	return
 
