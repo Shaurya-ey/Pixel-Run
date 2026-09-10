7d933d0cd781a2fc7faace2b6bbd2a97f499e19e
Shaurya Pandey <shauryaey246@gmail.com>
2026-09-10 11:59:26 +0530

    Done with basic structure of platformer

--- timer.gd
+++ timer.gd
@@ -1,4 +1,7 @@
-extends RichTextLabel
+extends Node2D
+@onready var timer: RichTextLabel = $timer
+
+var time: float 
 
 
 # Called when the node enters the scene tree for the first time.
@@ -7,5 +10,16 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	timer.text = str(snapped(time, 0.10))
+	
+func _Timer(start_time: float):
+
+	time = start_time
+	while time > 0.0:
+		await wait(0.10)
+		time = time - 0.10
+	return
+	
+func wait(seconds: float) -> void: 
+	await get_tree().create_timer(seconds).timeout
